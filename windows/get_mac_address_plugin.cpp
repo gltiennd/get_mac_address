362769eff1a1216defd88c7426486bc8809daf75
Duy Tien <duytient31@gmail.com>
2024-01-09 11:24:04 +0700

    Update get_mac_address_plugin.cpp

--- windows/get_mac_address_plugin.cpp
+++ windows/get_mac_address_plugin.cpp
@@ -57,57 +57,49 @@ void GetMacAddressPlugin::HandleMethodCall(
 }
 
 
-char* GetMacAddressPlugin::getMAC() const
-{
-  DWORD dwBufLen = sizeof(IP_ADAPTER_INFO);
-    char *mac_addr = static_cast<char*>(malloc(18));
-  auto AdapterInfo = static_cast<IP_ADAPTER_INFO*>(malloc(sizeof(IP_ADAPTER_INFO)));
-    if (AdapterInfo== nullptr){
+char* GetMacAddressPlugin::getMAC() const {
+    DWORD dwBufLen = sizeof(IP_ADAPTER_INFO);
+    char* mac_addr = static_cast<char*>(malloc(18));
+    auto AdapterInfo = static_cast<IP_ADAPTER_INFO*>(malloc(sizeof(IP_ADAPTER_INFO)));
+    if (AdapterInfo == nullptr) {
         printf("Error allocating memory needed to call GetAdaptersinfo\n");
         free(mac_addr);
-        return nullptr; // it is safe to call free(NULL)
+        return nullptr;
     }
-  // Make an initial call to GetAdaptersInfo to get the necessary size into the dwBufLen variable
-  if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == ERROR_BUFFER_OVERFLOW) {
+
+    // Make an initial call to GetAdaptersInfo to get the necessary size into the dwBufLen variable
+    if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == ERROR_BUFFER_OVERFLOW) {
+        free(AdapterInfo);
+        AdapterInfo = static_cast<IP_ADAPTER_INFO*>(malloc(dwBufLen));
+        if (AdapterInfo == nullptr) {
+            printf("Error allocating memory needed to call GetAdaptersinfo\n");
+            free(mac_addr);
+            return nullptr;
+        }
+    }
+
+    if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == NO_ERROR) {
+        // Contains pointer to current adapter info
+        PIP_ADAPTER_INFO pAdapterInfo = AdapterInfo;
+        do {
+            // Technically should look at pAdapterInfo->AddressLength
+            //   and not assume it is 6.
+            sprintf_s(mac_addr, 18, "%02X:%02X:%02X:%02X:%02X:%02X",
+                pAdapterInfo->Address[0], pAdapterInfo->Address[1],
+                pAdapterInfo->Address[2], pAdapterInfo->Address[3],
+                pAdapterInfo->Address[4], pAdapterInfo->Address[5]);
+
+            printf("Address: %s, mac: %s\n", pAdapterInfo->IpAddressList.IpAddress.String, mac_addr);
+
+            // Print them all, return the last one.
+            // Return mac_addr;
+
+            printf("\n");
+            pAdapterInfo = pAdapterInfo->Next;
+        } while (pAdapterInfo);
+    }
     free(AdapterInfo);
-    AdapterInfo = static_cast<IP_ADAPTER_INFO*>(malloc(dwBufLen));
-    if (AdapterInfo == nullptr) {
-      printf("Error allocating memory needed to call GetAdaptersinfo\n");
-      free(mac_addr);
-      return nullptr;
-    }
-  }
-
-  if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == NO_ERROR) {
-    // Contains pointer to current adapter info
-    PIP_ADAPTER_INFO pAdapterInfo = AdapterInfo;
-    do {
-      // technically should look at pAdapterInfo->AddressLength
-      //   and not assume it is 6.
-      //sprintf_s(mac_addr, "%02X:%02X:%02X:%02X:%02X:%02X",
-      //  pAdapterInfo->Address[0], pAdapterInfo->Address[1],
-      //  pAdapterInfo->Address[2], pAdapterInfo->Address[3],
-      //  pAdapterInfo->Address[4], pAdapterInfo->Address[5]);
-
-        char filename[1024];
-        char path1[128] = R"(D:\Program\Tesseract-OCR\tesseract.exe)";
-        char path2[128] = R"(D:\Program\Tesseract-OCR\)";
-        char path3[128] = R"(D:\Program\Tesseract-OCR\txt)";
-        char path4[128] = "-l chi_sim";
-        sprintf_s(filename,"%s %s %s %s",path1,path2,path3,path4);
-
-
-
-      printf("Address: %s, mac: %s\n", pAdapterInfo->IpAddressList.IpAddress.String, mac_addr);
-      // print them all, return the last one.
-      // return mac_addr;
-
-      printf("\n");
-      pAdapterInfo = pAdapterInfo->Next;
-    } while(pAdapterInfo);
-  }
-  free(AdapterInfo);
-  return mac_addr; // caller must free.
+    return mac_addr; // Caller must free.
 }
 
 
